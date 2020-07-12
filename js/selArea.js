d877d093f7c48d87a9c6320adc4d91fe8d97c916
CHENG HAN <y3226999@gmail.com>
2020-07-12 11:30:38 +0800

    add touch event

--- js/selArea.js
+++ js/selArea.js
@@ -2,6 +2,42 @@ function setSelArea() {
     var div = document.getElementById('sel_area'),
         x1 = 0, y1 = 0, x2 = 0, y2 = 0,
         is_click = false;
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        ontouchstart = function (e) {
+            if (e.cancelable) {
+                e.preventDefault();
+            }
+            onCursorDown(e.touches[0].clientX, e.touches[0].clientY)
+        }
+        ontouchmove = function (e) {
+            if (e.cancelable) {
+                e.preventDefault();
+            }
+            onCursorMove(e.touches[0].clientX, e.touches[0].clientY)
+        };
+        ontouchend = function (e) {
+            if (e.cancelable) {
+                e.preventDefault();
+            }
+            onCursorUp()
+        }
+    }
+    else {
+        onmousedown = function (e) {
+            e.preventDefault();
+            onCursorDown(e.clientX, e.clientY)
+        }
+        onmousemove = function (e) {
+            e.preventDefault();
+            onCursorMove(e.clientX, e.clientY)
+        };
+        onmouseup = function (e) {
+            e.preventDefault();
+            onCursorUp()
+        }
+    }
+
     function reCalc() {
         var x3 = Math.min(x1, x2);
         var x4 = Math.max(x1, x2);
@@ -12,24 +48,27 @@ function setSelArea() {
         div.style.width = x4 - x3 + 'px';
         div.style.height = y4 - y3 + 'px';
     }
-    onmousedown = function (e) {
+    function onCursorDown(x, y) {
+        console.log('click')
         var rect = app.elem.cns[0].getBoundingClientRect();
-        x1 = e.clientX
-        y1 = e.clientY
+        x1 = x
+        y1 = y
         if (!(x1 >= rect.left && x1 <= rect.right && y1 >= rect.top && y1 <= rect.bottom))
             return
-        x1 = e.clientX + window.scrollX;
-        y1 = e.clientY + window.scrollY;
+        x1 = x + window.scrollX;
+        y1 = y + window.scrollY;
         is_click = true
     };
-    onmousemove = function (e) {
+    function onCursorMove(x, y) {
+        console.log('move')
         if (!is_click) return
         div.hidden = 0;
-        x2 = e.clientX + window.scrollX;
-        y2 = e.clientY + window.scrollY;
+        x2 = x + window.scrollX;
+        y2 = y + window.scrollY;
         reCalc();
     };
-    onmouseup = function (e) {
+    function onCursorUp() {
+        console.log('up')
         if (!is_click) return
         is_click = false
         var rect = app.elem.cns[0].getBoundingClientRect();
@@ -43,24 +82,4 @@ function setSelArea() {
         app.elem.ctx[1].drawImage(app.elem.cns[0], x3, y3, x4 - x3, y4 - y3, 0, 0, x4 - x3, y4 - y3)
         app.edgeDetect()
     };
-
-
-    document.body.addEventListener('touchstart', function (e) {
-        triggerMouseEvent(this, "mousedown");
-    }
-        , true);
-    document.body.addEventListener('touchmove', function (e) {
-        triggerMouseEvent(this, "mousemove");
-    }
-        , true);
-    document.body.addEventListener('touchend', function () {
-        triggerMouseEvent(this, "mouseup");
-    }
-        , true);
-
-    function triggerMouseEvent(node, eventType) {
-        var clickEvent = document.createEvent('MouseEvents');
-        clickEvent.initEvent(eventType, true, true);
-        node.dispatchEvent(clickEvent);
-    }
 }
